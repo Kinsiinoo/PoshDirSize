3f690a286e808644b014d0ef8c6ee2956dfa8ab1
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2021-04-13 17:29:10 +0200

    Added "Add-ToFileList" function

--- Get-PoshDirSize.ps1
+++ Get-PoshDirSize.ps1
@@ -4,6 +4,8 @@ $DirTotalSize = $null
 $FileTotalSize = $null
 $PoshDSTotal = $null
 
+[System.Collections.Generic.List[Object]]$FileList = @()
+
 # Functions
 Function ConvertTo-FileSize {
     Param ([uint64]$Size)
@@ -13,4 +15,32 @@ Function ConvertTo-FileSize {
     ElseIf ($Size -gt 1KB) {[string]::Format("{0,12:N1} kB", $Size / 1KB)}
     ElseIf ($Size -gt 0) {[string]::Format("{0,12:N1} B", $Size)}
     Else {"Size not readable or null!"}
+}
+
+function Add-ToFileList {
+    param(
+        [Parameter(Mandatory=$true)][System.IO.FileInfo]$ds_file_item
+    )
+
+    $FullPath         = $null        
+    $FileObject       = $null
+    $FileSizeReadable = $null
+    $FileSize         = $null
+    $FileName         = $null
+
+    $FullPath = $ds_file_item.FullName
+    $FileName = $ds_file_item.BaseName
+
+    $FileSizeReadable   = (ConvertTo-FileSize $ds_file_item.Length)
+    $FileSize           = $ds_file_item.Length
+
+    $FileObject = [PSCustomObject]@{
+        PSTypeName    = 'PS.File.List.Result'
+        FileName      = $FileName
+        SizeReadable  = $FileSizeReadable  
+        SizeInBytes   = $FileSize
+        FullPath      = $FullPath
+    }
+
+    $FileList.Add($FileObject)
 }
