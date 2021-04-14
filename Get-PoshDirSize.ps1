3843df040ed32d59dcc393c3389d37e5e060f863
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2021-04-14 11:52:58 +0200

    Fixed filesize format

--- Get-PoshDirSize.ps1
+++ Get-PoshDirSize.ps1
@@ -11,11 +11,11 @@ $Dir_Items = Get-ChildItem $PoshDSPath -Directory -Recurse
 # Functions
 Function ConvertTo-FileSize {
     Param ([uint64]$Size)
-    If ($Size -gt 1TB) {[string]::Format("{0,12:N1} TB", $Size / 1TB)}
-    ElseIf ($Size -gt 1GB) {[string]::Format("{0,12:N1} GB", $Size / 1GB)}
-    ElseIf ($Size -gt 1MB) {[string]::Format("{0,12:N1} MB", $Size / 1MB)}
-    ElseIf ($Size -gt 1KB) {[string]::Format("{0,12:N1} kB", $Size / 1KB)}
-    ElseIf ($Size -gt 0) {[string]::Format("{0,12:N1} B", $Size)}
+    If ($Size -gt 1TB) {[string]::Format("{0,9:N2} TB", $Size / 1TB)}
+    ElseIf ($Size -gt 1GB) {[string]::Format("{0,9:N2} GB", $Size / 1GB)}
+    ElseIf ($Size -gt 1MB) {[string]::Format("{0,9:N2} MB", $Size / 1MB)}
+    ElseIf ($Size -gt 1KB) {[string]::Format("{0,9:N2} KB", $Size / 1KB)}
+    ElseIf ($Size -gt 0) {[string]::Format("{0,9:N2} B", $Size)}
     Else {"Size not readable or null!"}
 }
 
