956f9de3b4b209a83e4462eaef8bdf136df3527e
Kinsiinoo <20649992+Kinsiinoo@users.noreply.github.com>
2021-04-17 14:13:26 +0200

    PS Module: Formatting code into one function

--- Get-PoshDirSize.ps1
+++ Get-PoshDirSize.ps1
@@ -1,125 +1,133 @@
-﻿# Variables
-$PoshDSPath = ([Management.Automation.WildcardPattern]::Escape("C:\"))
-$PoshDSOutPath = "C:\Temp\PoshDirSize\"
-$PoshDSTotal = $null
-$PoshDSRunTime = (Get-Date).ToString('yyyy-MM-dd-HH-mm-ss')
-
-[System.Collections.Generic.List[Object]]$FileList = @()
-[System.Collections.Generic.List[Object]]$DirList = @()
-
-# SW start
-$PoshDSSW = [Diagnostics.Stopwatch]::StartNew()
-
-$File_Items = Get-ChildItem $PoshDSPath -File -Recurse
-$Dir_Items = Get-ChildItem $PoshDSPath -Directory -Recurse
-
-# Functions
-Function ConvertTo-FileSize {
-    Param ([uint64]$Size)
-    If ($Size -gt 1TB) {[string]::Format("{0,9:N2} TB", $Size / 1TB)}
-    ElseIf ($Size -gt 1GB) {[string]::Format("{0,9:N2} GB", $Size / 1GB)}
-    ElseIf ($Size -gt 1MB) {[string]::Format("{0,9:N2} MB", $Size / 1MB)}
-    ElseIf ($Size -gt 1KB) {[string]::Format("{0,9:N2} KB", $Size / 1KB)}
-    ElseIf ($Size -gt 0) {[string]::Format("{0,9:N2} B", $Size)}
-    Else {"Size not readable or null!"}
-}
-
-function Add-ToFileList {
-    param(
-        [Parameter(Mandatory=$true)][System.IO.FileInfo]$file_item
+﻿function Get-PoshDirSize {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,Position=0,
+        HelpMessage="Enter the path to the folder you want to check.")]
+        [String]$PoshDSPath
     )
 
-    $FullPath         = $null        
-    $FileObject       = $null
-    $FileSizeReadable = $null
-    $FileSize         = $null
-    $FileName         = $null
+    # Variables
+    $PoshDSOutPath = "C:\Temp\PoshDirSize\"
+    $PoshDSTotal = $null
+    $PoshDSRunTime = (Get-Date).ToString('yyyy-MM-dd-HH-mm-ss')
 
-    $FullPath = $file_item.FullName
-    $FileName = $file_item.BaseName
+    [System.Collections.Generic.List[Object]]$FileList = @()
+    [System.Collections.Generic.List[Object]]$DirList = @()
 
-    $FileSizeReadable   = (ConvertTo-FileSize $file_item.Length)
-    $FileSize           = $file_item.Length
+    # SW start
+    $PoshDSSW = [Diagnostics.Stopwatch]::StartNew()
 
-    $FileObject = [PSCustomObject]@{
-        PSTypeName    = 'PS.File.List.Result'
-        FileName      = $FileName
-        SizeReadable  = $FileSizeReadable  
-        SizeInBytes   = $FileSize
-        FullPath      = $FullPath
+    $File_Items = Get-ChildItem $PoshDSPath -File -Recurse
+    $Dir_Items = Get-ChildItem $PoshDSPath -Directory -Recurse
+
+    # Functions
+    Function ConvertTo-FileSize {
+        Param ([uint64]$Size)
+        If ($Size -gt 1TB) {[string]::Format("{0,9:N2} TB", $Size / 1TB)}
+        ElseIf ($Size -gt 1GB) {[string]::Format("{0,9:N2} GB", $Size / 1GB)}
+        ElseIf ($Size -gt 1MB) {[string]::Format("{0,9:N2} MB", $Size / 1MB)}
+        ElseIf ($Size -gt 1KB) {[string]::Format("{0,9:N2} KB", $Size / 1KB)}
+        ElseIf ($Size -gt 0) {[string]::Format("{0,9:N2} B", $Size)}
+        Else {"Size not readable or null!"}
     }
 
-    $FileList.Add($FileObject)
-}
+    function Add-ToFileList {
+        param(
+            [Parameter(Mandatory=$true)][System.IO.FileInfo]$file_item
+        )
 
-function Add-ToDirList {
-    param(
-        [Parameter(Mandatory=$true)][System.IO.DirectoryInfo]$folder_item,
-        [Parameter(Mandatory=$true)][Double]$folder_item_bytes,
-        [Parameter(Mandatory=$true)][String]$folder_item_size
-    )
+        $FullPath         = $null        
+        $FileObject       = $null
+        $FileSizeReadable = $null
+        $FileSize         = $null
+        $FileName         = $null
 
-    $FullPath           = $null        
-    $FolderObject       = $null
-    $FolderSizeReadable = $null
-    $FolderSize         = $null
-    $FolderName         = $null
+        $FullPath = $file_item.FullName
+        $FileName = $file_item.BaseName
 
-    $FullPath   = $folder_item.FullName
-    $FolderName = $folder_item.BaseName
+        $FileSizeReadable   = (ConvertTo-FileSize $file_item.Length)
+        $FileSize           = $file_item.Length
 
-    $FolderSizeReadable   = $folder_item_size
-    $FolderSize           = $folder_item_bytes
+        $FileObject = [PSCustomObject]@{
+            PSTypeName    = 'PS.File.List.Result'
+            FileName      = $FileName
+            SizeReadable  = $FileSizeReadable  
+            SizeInBytes   = $FileSize
+            FullPath      = $FullPath
+        }
 
-    $FolderObject = [PSCustomObject]@{
-        PSTypeName       = 'PS.File.List.Result'
-        FolderName       = $FolderName
-        SizeReadable     = $FolderSizeReadable
-        SizeInBytes      = $FolderSize
-        FullPath         = $FullPath
+        $FileList.Add($FileObject)
     }
 
-    $DirList.Add($FolderObject)
+    function Add-ToDirList {
+        param(
+            [Parameter(Mandatory=$true)][System.IO.DirectoryInfo]$folder_item,
+            [Parameter(Mandatory=$true)][Double]$folder_item_bytes,
+            [Parameter(Mandatory=$true)][String]$folder_item_size
+        )
+
+        $FullPath           = $null        
+        $FolderObject       = $null
+        $FolderSizeReadable = $null
+        $FolderSize         = $null
+        $FolderName         = $null
+
+        $FullPath   = $folder_item.FullName
+        $FolderName = $folder_item.BaseName
+
+        $FolderSizeReadable   = $folder_item_size
+        $FolderSize           = $folder_item_bytes
+
+        $FolderObject = [PSCustomObject]@{
+            PSTypeName       = 'PS.File.List.Result'
+            FolderName       = $FolderName
+            SizeReadable     = $FolderSizeReadable
+            SizeInBytes      = $FolderSize
+            FullPath         = $FullPath
+        }
+
+        $DirList.Add($FolderObject)
+    }
+
+    # Dir_Item -> DirList
+    foreach($Dir_Item in $Dir_Items){
+        $Dir_ChildItems = Get-ChildItem ([Management.Automation.WildcardPattern]::Escape($Dir_Item.FullName)) -Recurse
+        $Dir_Bytes = ($Dir_ChildItems | Measure-Object -Property Length -sum).Sum
+        $Dir_Size = (ConvertTo-FileSize $Dir_Bytes)
+        Add-ToDirList $Dir_Item $Dir_Bytes $Dir_Size
+    }
+
+    # File_Item -> FileList
+    foreach($File_Item in $File_Items){
+        $PoshDSTotal += $File_Item.Length
+        Add-ToFileList $File_Item
+    }
+
+    # Test Path and Create folder if it is not exist
+    If(!(Test-Path -Path $PoshDSOutPath)){
+        New-Item -ItemType Directory -Force -Path $PoshDSOutPath
+    }
+
+    # DirList output to console and .log
+    $DirList | Sort-Object -Property SizeInBytes -Descending | Select-Object -Property FolderName, SizeReadable, SizeInBytes | Format-Table -AutoSize
+    $DirList | Sort-Object -Property SizeInBytes -Descending | Out-File -FilePath "$($PoshDSOutPath)\PoshDirSize_$($PoshDSRunTime).log" -Encoding utf8 -Append -Width 1000
+
+    # FileList output to console and .log
+    $FileList | Sort-Object -Property SizeInBytes -Descending | Select-Object -Property FileName, SizeReadable, SizeInBytes | Format-Table -AutoSize
+    $FileList | Sort-Object -Property SizeInBytes -Descending | Out-File -FilePath "$($PoshDSOutPath)\PoshDirSize_$($PoshDSRunTime).log" -Encoding utf8 -Append -Width 1000
+
+    # SW stop
+    $PoshDSSW.Stop()
+
+    # Path, Grand Total, Elapsed time output to console and .log
+    Write-Host "`nPath: " -NoNewline -ForegroundColor Cyan
+    $PoshDSPath
+
+    Write-Host "Grand Total: " -NoNewline -ForegroundColor DarkYellow
+    ConvertTo-FileSize $PoshDSTotal
+
+    Write-Host "Elapsed time: " -NoNewline -ForegroundColor Magenta
+    Write-Host "$($PoshDSSW.Elapsed.Hours):$($PoshDSSW.Elapsed.Minutes):$($PoshDSSW.Elapsed.Seconds).$($PoshDSSW.Elapsed.Milliseconds)"
+
+    "Path: $($PoshDSPath)`nGrand Total: $(ConvertTo-FileSize $PoshDSTotal)`nElapsed time: $($PoshDSSW.Elapsed.Hours):$($PoshDSSW.Elapsed.Minutes):$($PoshDSSW.Elapsed.Seconds).$($PoshDSSW.Elapsed.Milliseconds)" | Out-File -FilePath "$($PoshDSOutPath)\PoshDirSize_$($PoshDSRunTime).log" -Encoding utf8 -Append -Width 1000 
 }
-
-# Dir_Item -> DirList
-foreach($Dir_Item in $Dir_Items){
-    $Dir_ChildItems = Get-ChildItem ([Management.Automation.WildcardPattern]::Escape($Dir_Item.FullName)) -Recurse
-    $Dir_Bytes = ($Dir_ChildItems | Measure-Object -Property Length -sum).Sum
-    $Dir_Size = (ConvertTo-FileSize $Dir_Bytes)
-    Add-ToDirList $Dir_Item $Dir_Bytes $Dir_Size
-}
-
-# File_Item -> FileList
-foreach($File_Item in $File_Items){
-    $PoshDSTotal += $File_Item.Length
-    Add-ToFileList $File_Item
-}
-
-# Test Path and Create folder if it is not exist
-If(!(Test-Path -Path $PoshDSOutPath)){
-    New-Item -ItemType Directory -Force -Path $PoshDSOutPath
-}
-
-# DirList output to console and .log
-$DirList | Sort-Object -Property SizeInBytes -Descending | Select-Object -Property FolderName, SizeReadable, SizeInBytes | Format-Table -AutoSize
-$DirList | Sort-Object -Property SizeInBytes -Descending | Out-File -FilePath "$($PoshDSOutPath)\PoshDirSize_$($PoshDSRunTime).log" -Encoding utf8 -Append -Width 1000
-
-# FileList output to console and .log
-$FileList | Sort-Object -Property SizeInBytes -Descending | Select-Object -Property FileName, SizeReadable, SizeInBytes | Format-Table -AutoSize
-$FileList | Sort-Object -Property SizeInBytes -Descending | Out-File -FilePath "$($PoshDSOutPath)\PoshDirSize_$($PoshDSRunTime).log" -Encoding utf8 -Append -Width 1000
-
-# SW stop
-$PoshDSSW.Stop()
-
-# Path, Grand Total, Elapsed time output to console and .log
-Write-Host "`nPath: " -NoNewline -ForegroundColor Cyan
-$PoshDSPath
-
-Write-Host "Grand Total: " -NoNewline -ForegroundColor DarkYellow
-ConvertTo-FileSize $PoshDSTotal
-
-Write-Host "Elapsed time: " -NoNewline -ForegroundColor Magenta
-Write-Host "$($PoshDSSW.Elapsed.Hours):$($PoshDSSW.Elapsed.Minutes):$($PoshDSSW.Elapsed.Seconds).$($PoshDSSW.Elapsed.Milliseconds)"
-
-"Path: $($PoshDSPath)`nGrand Total: $(ConvertTo-FileSize $PoshDSTotal)`nElapsed time: $($PoshDSSW.Elapsed.Hours):$($PoshDSSW.Elapsed.Minutes):$($PoshDSSW.Elapsed.Seconds).$($PoshDSSW.Elapsed.Milliseconds)" | Out-File -FilePath "$($PoshDSOutPath)\PoshDirSize_$($PoshDSRunTime).log" -Encoding utf8 -Append -Width 1000 
